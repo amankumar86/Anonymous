147571da44e4c351a67b2956c48642cc9453e13a
AS <59010081+iArchit@users.noreply.github.com>
2020-05-11 17:01:02 +0700

    Delete index.ts

--- Anonymous Chat/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { createPeerConnection, CreatePeerConnectionProps, CreatePeerConnectionResponse } from './createPeerConnection';
-
-export { createPeerConnection, CreatePeerConnectionProps, CreatePeerConnectionResponse };
