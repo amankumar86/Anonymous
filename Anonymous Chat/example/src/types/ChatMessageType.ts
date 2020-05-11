9fe57f8f3faf46fc4da4cffd27851b17291fdf41
AS <59010081+iArchit@users.noreply.github.com>
2020-05-11 17:08:07 +0700

    Delete ChatMessageType.ts

--- Anonymous Chat/example/src/types/ChatMessageType.ts
+++
@@ -1,9 +0,0 @@
-import { MESSAGE_SENDER } from './MessageSenderEnum';
-
-export type ChatMessageType = {
-  id: string;
-  sender: MESSAGE_SENDER;
-  timestamp: number;
-  text?: string;
-  fileId?: string;
-};
